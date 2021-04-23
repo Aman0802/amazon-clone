f733030a4fe5791480238c67672c70a88bf309a0
Aman0802 <aman.khubani@gmail.com>
2021-04-23 18:37:20 +0530

    cra setup and amazon header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDd1TXXVX5_hB8F_JKqBKZhAIOcLt_qKuk",
+  authDomain: "clone-5eeee.firebaseapp.com",
+  projectId: "clone-5eeee",
+  storageBucket: "clone-5eeee.appspot.com",
+  messagingSenderId: "916979165963",
+  appId: "1:916979165963:web:32fdd1d131fc5b6e0a900e",
+  measurementId: "G-RR6PF34YM1",
+};
